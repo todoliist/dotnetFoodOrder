0bc800532610c6b7d7f6fae4efd43d1430e1f031
ssu851009 <ssu851009@hotmail.com>
2021-08-18 13:42:54 -0600

    add home and details page

--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Mango.Web.Models
 {
     public class ProductDto
     {
+        public ProductDto()
+        {
+            Count = 1;
+        }
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        [Range(1,100)]
+        public int Count { get; set; }
     }
 }
