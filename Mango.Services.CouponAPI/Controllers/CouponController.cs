176680e401ff2d5313dfdc55588bb02d3a5bafee
ssu851009 <ssu851009@hotmail.com>
2021-08-26 16:10:02 -0600

    create coupon API

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,40 @@
+﻿using Mango.Services.CouponAPI.Models.Dto;
+using Mango.Services.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/coupon")]
+    public class CouponController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private ICouponRepository _couponRepository;
+        
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet("{code}")]
+        public async Task<object> Get(string code)
+        {
+            try
+            {
+                var coupon = await _couponRepository.GetCouponByCode(code);
+                _response.Result = coupon;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
